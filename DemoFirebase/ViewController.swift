ddc4ec29f7f1a07cd87861973d0cd7a608f3c8ac
Sam Lee <sam1990790120@gmail.com>
2021-09-05 15:51:38 +0800

    Initial Commit

---
+++ DemoFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoFirebase
+//
+//  Created by 李世文 on 2021/9/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
