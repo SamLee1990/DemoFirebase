6a03bc11a63da1e8b09bff981b0780cced9bb060
Sam Lee <sam1990790120@gmail.com>
2021-09-12 18:13:38 +0800

    補 Assets.xcassets

--- DemoFirebase/LoginViewController.swift
+++ DemoFirebase/LoginViewController.swift
@@ -189,14 +189,3 @@ extension LoginViewController: LoginButtonDelegate {
     
 
 }
-
-
-
-
-// Swift // // Add this to the header of your file, e.g. in ViewController.swift import FBSDKLoginKit // Add this to the body class ViewController:UIViewController { override func viewDidLoad() { super.viewDidLoad() let loginButton = FBLoginButton() loginButton.center = view.center view.addSubview(loginButton) } }
-
-// Swift override func viewDidLoad() { super.viewDidLoad() if let token = AccessToken.current, !token.isExpired { // User is logged in, do work such as go to next view controller. } }
-    
-// Swift // // Extend the code sample from 6a.Add Facebook Login to Your Code // Add to your viewDidLoad method: loginButton.permissions = ["public_profile", "email"]
-
-
